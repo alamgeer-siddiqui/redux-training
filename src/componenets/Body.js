427dd8c5a7ac4961c393fc6332a84abb508a18c5
Alamgeer Siddiqui <alamgeer.siddiqui@qsstechnosoft.com>
2022-07-26 16:04:25 +0530

    refact

--- src/componenets/Body.js
+++ src/componenets/Body.js
@@ -1,10 +1,9 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-// import {depositBalance,withdrawBalance} from "./action/index"
-import { depositBalance,withdrawBalance } from "../action/index";
+import { depositBalance, withdrawBalance } from "../action/index";
 
 export default function Body() {
-  const dispatch=useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className="position-absolute top-50 start-50 translate-middle">
       <div
@@ -12,13 +11,21 @@ export default function Body() {
         role="group"
         aria-label="Basic mixed styles example"
       >
-        <button type="button" className="btn btn-danger mx-2" onClick={()=>dispatch(withdrawBalance)}>
+        <button
+          type="button"
+          className="btn btn-danger mx-2"
+          onClick={() => dispatch(withdrawBalance)}
+        >
           -
         </button>
         <button type="button" disabled={true} className="btn btn-light">
           withdraw/deposit
         </button>
-        <button type="button" className="btn btn-success mx-2" onClick={()=>dispatch(depositBalance)}>
+        <button
+          type="button"
+          className="btn btn-success mx-2"
+          onClick={() => dispatch(depositBalance)}
+        >
           +
         </button>
       </div>
