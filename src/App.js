559823ca2f896ad409a1496c5d62be7ce53d2910
Alamgeer Siddiqui <alamgeer.siddiqui@qsstechnosoft.com>
2022-07-26 15:28:47 +0530

    refact

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./App.css";
 import Navbar from "./componenets/Navbar";
 import Body from "./componenets/Body";
-import store from "./store";
 
 export default function App() {
   return (
